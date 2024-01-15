77b19baec3ab66ccfa21ca3a25f02561a9807a52
valerylot <lotvalery@gmail.com>
2024-01-14 17:00:55 -0800

    edit to add ninja bonus

--- server.py
+++ server.py
@@ -10,14 +10,23 @@ def visits():
         session["count"]+=1
     return render_template("index.html")
 
-@app.route('/plus_two', methods=['POST'])
-def plus_two():
-    customize=int(request.form.get("custom", 1))
+@app.route('/change', methods=['POST'])
+def change():
     if request.form["change"]=="add":
-        session["count"] += customize
+        session["count"] += 1
     elif request.form["change"]=="restart":
         session["count"] = 0
     return redirect("/")
+
+# attempt at sensei bonus
+# @app.route('/plus_two', methods=['POST'])
+# def plus_two():
+#     customize=int(request.form.get("custom", 1))
+#     if request.form["change"]=="add":
+#         session["count"] += customize
+#     elif request.form["change"]=="restart":
+#         session["count"] = 0
+#     return redirect("/")
 
 @app.route('/destroy_session')
 def destroy():
